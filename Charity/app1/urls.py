69c84d1e3eca9efafa725683068f4fe775b0e52d
Wissam-Hawc <wissam.hawcharw@gmail.com>
2023-06-12 23:05:54 +0300

    w

--- Charity/app1/urls.py
+++ Charity/app1/urls.py
@@ -1,11 +1,10 @@
 # app1/urls.py
 
 from django.urls import path
-from . import views
-from .views import contact, about, programs, login_view, logout_user,register, stripePay
+from .views import contact, about, programs, login_view, logout_user, register, stripePay, home, program_details
 
 urlpatterns = [
-    path('', views.home, name='home'),
+    path('', home, name='home'),
     path('programs/', programs, name='programs'),
     path('about/', about, name='about'),
     path('contact/', contact, name='contact'),
@@ -13,7 +12,7 @@ urlpatterns = [
     path('logout_user', logout_user, name='logout'),
     path('register/', register, name='register'),
     path('donate/', stripePay, name='donate'),
-    path('programs/<int:program_id>/', views.program_details, name='program_details'),
-    path('register/', views.register, name='register'),
+    path('programs/<int:program_id>/',program_details, name='program_details'),
+    path('register/', register, name='register'),
 
 ]
